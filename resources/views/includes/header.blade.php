3899c8b7772487d565c7dd19cbb26472d2129168
Alex Skripov <scripoff.a@yandex.ru>
2025-04-12 20:15:03 +0300

    1

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сайт студенческих иницатив</title>
+    <link rel="stylesheet" href="{{ asset('public/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
+    
+</head>
+<body>
